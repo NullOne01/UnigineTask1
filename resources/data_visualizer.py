68bc9873762082bae1f4e22178ea5a377206943e
nullone <adviventes@gmail.com>
2022-08-05 15:42:54 +0300

    Added draw_cell_list to data_visualizer.py

--- resources/data_visualizer.py
+++ resources/data_visualizer.py
@@ -3,57 +3,98 @@ import numpy as np
 from matplotlib.patches import Wedge
 import matplotlib.pyplot as plt
 
-data_file_name = str(sys.argv[1])
-data_file = open(data_file_name, 'r')
 
-line = data_file.readline()
+def draw_cell_list(bottom_left, top_right, side):
+    bottom_right = [top_right[0], bottom_left[1]]
+    top_left = [bottom_left[0], top_right[1]]
 
-sector_angle = float(line.split()[0])
-view_distance = float(line.split()[1])
+    sides = np.array([bottom_left, bottom_right, top_right, top_left, bottom_left])
+    plt.plot(sides[:, 0], sides[:, 1])
 
-print(f"View distance: {view_distance}")
-print(f"Sector angle: {sector_angle}")
+    height = top_left[1] - bottom_right[1]
+    width = bottom_right[0] - top_left[0]
 
-origin_list = []
-view_list = []
-while True:
-    # Get next line from file
+    for i in range(int(height / side)):
+        new_y = bottom_left[1] + (i + 1) * side
+        plt.plot([bottom_left[0], bottom_right[0]], [new_y, new_y], color='orange')
+    for i in range(int(width / side)):
+        new_x = top_left[0] + (i + 1) * side
+        plt.plot([new_x, new_x], [bottom_left[1], top_left[1]], color='orange')
+
+
+def main():
+    data_file_name = str(sys.argv[1])
+    data_file = open(data_file_name, 'r')
+
     line = data_file.readline()
 
-    # if line is empty
-    # end of file is reached
-    if not line:
-        break
+    sector_angle = float(line.split()[0])
+    view_distance = float(line.split()[1])
 
-    sub_lines = line.split()
-    origin_list.append((float(sub_lines[0]), float(sub_lines[1])))
-    view_list.append((float(sub_lines[2]), float(sub_lines[3])))
+    print(f"View distance: {view_distance}")
+    print(f"Sector angle: {sector_angle}")
 
-data_file.close()
+    origin_list = []
+    view_list = []
+    while True:
+        # Get next line from file
+        line = data_file.readline()
 
-origin_arr = np.array(origin_list)
-view_arr = np.array(view_list)
+        # if line is empty
+        # end of file is reached
+        if not line:
+            break
 
-# View vector
-plt.quiver(origin_arr[:, 0], origin_arr[:, 1], view_arr[:, 0] * view_distance, view_arr[:, 1] * view_distance,
-           angles='xy', scale_units='xy', scale=1)
-# Unit pos
-plt.plot(origin_arr[:, 0], origin_arr[:, 1], 'go')
+        sub_lines = line.split()
+        origin_list.append((float(sub_lines[0]), float(sub_lines[1])))
+        view_list.append((float(sub_lines[2]), float(sub_lines[3])))
 
-# Sector
-fig = plt.figure(1)
-ax = fig.gca()
-right_vec = [1, 0]
-for i in range(len(origin_list)):
-    origin_pair = origin_list[i]
-    view_pair = view_arr[i]
+    data_file.close()
 
-    angle = np.math.atan2(np.linalg.det([right_vec, view_pair]), np.dot(right_vec, view_pair))
-    angle_deg = np.degrees(angle)
+    origin_arr = np.array(origin_list)
+    view_arr = np.array(view_list)
 
-    fov = Wedge(origin_pair, view_distance, angle_deg - sector_angle / 2, angle_deg + sector_angle / 2, color="r", alpha=0.5)
-    ax.add_artist(fov)
+    # View vector
+    plt.quiver(origin_arr[:, 0], origin_arr[:, 1], view_arr[:, 0] * view_distance, view_arr[:, 1] * view_distance,
+               angles='xy', scale_units='xy', scale=1)
+    # Unit pos
+    plt.plot(origin_arr[:, 0], origin_arr[:, 1], 'go')
 
-ax.set_aspect('equal')
-plt.tight_layout()
-plt.show()
+    # Sector
+    fig = plt.figure(1)
+    ax = fig.gca()
+    right_vec = [1, 0]
+
+    min_x = origin_arr[0][0]
+    min_y = origin_arr[0][0]
+    max_x = origin_arr[0][0]
+    max_y = origin_arr[0][0]
+
+    for i in range(len(origin_list)):
+        origin_pair = origin_list[i]
+        view_pair = view_arr[i]
+
+        min_x = min(min_x, origin_pair[0])
+        min_y = min(min_y, origin_pair[1])
+        max_x = max(max_x, origin_pair[0])
+        max_y = max(max_y, origin_pair[1])
+
+        angle = np.math.atan2(np.linalg.det([right_vec, view_pair]), np.dot(right_vec, view_pair))
+        angle_deg = np.degrees(angle)
+
+        fov = Wedge(origin_pair, view_distance, angle_deg - sector_angle / 2, angle_deg + sector_angle / 2, color="r",
+                    alpha=0.5)
+        ax.add_artist(fov)
+
+    # Drawing cell list
+    draw_cell_list([min_x, min_y], [max_x, max_y], view_distance)
+
+    plt.xlim([min_x - view_distance, max_x + view_distance])
+    plt.ylim([min_y - view_distance, max_y + view_distance])
+    ax.set_aspect('equal')
+    plt.tight_layout()
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
