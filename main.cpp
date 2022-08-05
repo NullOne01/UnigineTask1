3a3eed35147e1e104210c2a4edd10c3e0dc61213
nullone <adviventes@gmail.com>
2022-08-05 03:48:50 +0300

    Now game_info should load from disk fine

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main() {
     GameLoader game_loader;
-    game_loader.readGameInfo("resources/data1.txt");
+    auto game_info = game_loader.readGameInfo("resources/data1.txt");
 
     std::cout << "Hello, World!" << std::endl;
 
--- view/GameLoader.cpp
+++ view/GameLoader.cpp
@@ -5,16 +5,19 @@
 GameInfo GameLoader::readGameInfo(const std::string &file_name) {
     std::ifstream file_stream(file_name);
     if (!file_stream.is_open()) {
-        std::throw_with_nested( std::runtime_error("File not found") );
+        std::throw_with_nested(std::runtime_error("File not found"));
     }
 
     double sector_angle, view_distance;
     file_stream >> sector_angle >> view_distance;
 
     std::vector<std::unique_ptr<Unit>> units;
-    units.resize(MEDIUM_NUMBER_OF_UNITS);
+    units.reserve(MEDIUM_NUMBER_OF_UNITS);
 
-    double min_x, min_y, max_x, max_y;
+    double min_x = std::numeric_limits<double>::infinity();
+    double min_y = std::numeric_limits<double>::infinity();
+    double max_x = -std::numeric_limits<double>::infinity();
+    double max_y = -std::numeric_limits<double>::infinity();
     double pos_x, pos_y, view_x, view_y;
     while (file_stream >> pos_x >> pos_y >> view_x >> view_y) {
         units.push_back(std::make_unique<Unit>(pos_x, pos_y, view_x, view_y));
@@ -28,5 +31,10 @@ GameInfo GameLoader::readGameInfo(const std::string &file_name) {
     file_stream.close();
 
     GameInfo game_info(sector_angle, view_distance, {min_x, min_y}, {max_x, max_y});
+    game_info.units = std::move(units);
+    for (auto &unit: game_info.units) {
+        game_info.world_map.addItem(unit.get(), unit->pos);
+    }
+
     return game_info;
 }
